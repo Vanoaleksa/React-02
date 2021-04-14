df717d440a9bbd3031c1cd1109eed5fcf4155bbc
Vanoaleksa <vanoaleksa111@yandex.by>
2021-04-14 20:44:39 +0300

    Adding structure for Messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,33 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) =>{
     return(
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+
+                <div className={s.dialog + ' ' + s.active}>
+                    Lionel
+                </div>
+                <div className={s.dialog}>
+                    Suares
+                </div>
+                <div className={s.dialog}>
+                    Neymar
+                </div>
+                <div className={s.dialog}>
+                    Ronaldinho
+                </div>
+                <div className={s.dialog}>
+                    Ter Stegen
+                </div>
+                <div className={s.dialog}>
+                    Grizman
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>F*ck Real!</div>
+                <div className={s.message}>We are champions!</div>
+                <div className={s.message}>Do you smoke?</div>
+            </div>
         </div>
     );
 }
