ee13bd653c4dc221497624ab5133e4d05d470d33
Vanoaleksa <vanoaleksa111@yandex.by>
2021-04-27 19:46:30 +0300

    Packing data into 'state'

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = (props) => {
   return (
     <div>
       <ProfileInfo />
-      <Myposts posts={props.posts} />
+      <Myposts posts={props.state.posts} />
     </div>
   );
 }
---
+++ src/redux/state.js
@@ -0,0 +1,34 @@
+
+let state = {
+    profilePage: {
+        posts: [
+            { id: '1', message: 'Visca el Barca!', likecounts: '100' },
+            { id: '2', message: 'Visca el Catalunya!', likecounts: '30' }
+        ],
+
+        
+    },
+
+    dialogsPage: {
+        messages: [
+
+            { id: '1', message: 'F*ck Real!' },
+            { id: '2', message: 'We are champions!' },
+            { id: '3', message: 'Do you smoke?' }
+
+        ],
+        dialogs: [
+
+            { id: '1', name: 'Lionel' },
+            { id: '2', name: 'Suares' },
+            { id: '3', name: 'Neymar' },
+            { id: '4', name: 'Ronaldinho' },
+            { id: '5', name: 'Ter Stegen' },
+            { id: '6', name: 'Griezman' }
+
+        ]
+
+    }
+}
+
+export default state;
